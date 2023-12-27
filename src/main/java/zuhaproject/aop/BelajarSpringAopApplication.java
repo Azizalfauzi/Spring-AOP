6bcf90496246da05a6e157594af6d099a35a0e4c
Aziz Alfa <azizalfauzi65@gmail.com>
2023-12-27 10:14:49 +0700

    * Spring Boot AOP - Mengaktifkan AspectJ

--- src/main/java/zuhaproject/aop/BelajarSpringAopApplication.java
+++ src/main/java/zuhaproject/aop/BelajarSpringAopApplication.java
@@ -2,13 +2,14 @@ package zuhaproject.aop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-
+@EnableAspectJAutoProxy
 @SpringBootApplication
 public class BelajarSpringAopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BelajarSpringAopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BelajarSpringAopApplication.class, args);
+    }
 
 }
