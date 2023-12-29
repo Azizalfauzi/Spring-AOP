222172280984b85d130e3cc4002001788b6e032f
Aziz Alfa <azizalfauzi65@gmail.com>
2023-12-29 10:54:23 +0700

    *Spring Boot AOP - Pointcut expression format

--- src/main/java/zuhaproject/aop/aspect/LogAspect.java
+++ src/main/java/zuhaproject/aop/aspect/LogAspect.java
@@ -2,6 +2,8 @@ package zuhaproject.aop.aspect;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -22,10 +24,29 @@ public class LogAspect {
         String methodName = joinPoint.getSignature().getName();
         log.info("Before" + className + "." + methodName + "()");
     }
+    @Around("helloServiceMethod()")
+    public Object aroundHelloServiceMethod(ProceedingJoinPoint joinPoint) throws Throwable {
+        String className = joinPoint.getTarget().getClass().getName();
+        String methodName = joinPoint.getSignature().getName();
+        try {
+            log.info("Around Before" + className + "." + methodName + "()");
+            return joinPoint.proceed(joinPoint.getArgs());
+        } catch (Throwable throwable) {
+            log.info("Around Error" + className + "." + methodName + "()");
+            throw throwable;
+        } finally {
+            log.info("Around Finally" + className + "." + methodName + "()");
+        }
+    }
 
-    @Before("helloServiceMethod()")
-    public void beforeHelloServiceMethod2() {
-        log.info("Before HelloService method again");
-        log.info("Test error merge");
+    @Pointcut("execution(* zuhaproject.aop.services.HelloService.*(java.lang.String))")
+    public void pointcutHelloServiceStringParam() {
+
+    }
+
+    @Before("pointcutHelloServiceStringParam()")
+    public void logStringParameter(JoinPoint joinPoint) {
+        String value = (String) joinPoint.getArgs()[0];
+        log.info("Execute method with parameter : " + value);
     }
 }
