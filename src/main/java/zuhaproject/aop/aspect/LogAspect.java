dc1abd101b509b7701e8458c6695265ef1d7e010
Aziz Alfa <azizalfauzi65@gmail.com>
2023-12-29 09:50:08 +0700

    * Spring Boot AOP - AspectJ advice

--- src/main/java/zuhaproject/aop/aspect/LogAspect.java
+++ src/main/java/zuhaproject/aop/aspect/LogAspect.java
@@ -1,14 +1,27 @@
 package zuhaproject.aop.aspect;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Slf4j
 public class LogAspect {
 
-    @Pointcut("")
+    @Pointcut("target(zuhaproject.aop.services.HelloService)")
     public void helloServiceMethod() {
+    }
+
+    @Before("helloServiceMethod()")
+    public void beforeHelloServiceMethod() {
+        log.info("Before HelloService method");
+    }
+
+    @Before("helloServiceMethod()")
+    public void beforeHelloServiceMethod2() {
+        log.info("Before HelloService method again");
     }
 }
